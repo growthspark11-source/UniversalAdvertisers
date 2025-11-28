4bd24e4664672ee022420b12438075a27ac0f71c
ashutosh <ashutosh2013official@gmail.com>
2025-11-28 17:12:55 +0530

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://www.technove24.info',
-  base: '/universaladvertiser/', // this ensures all asset paths include the repo path
+  site: 'https://www.universalad.in',
+  base: '/', // this ensures all asset paths include the repo path
   outDir: 'dist',
   vite: {
     plugins: [tailwindcss()],
